0ff6957a0893855ff65c94665b6ea88fa2f90557
gko <gko@debian>
2021-07-06 18:24:38 +0200

    create project

---
+++ src/main/java/gilko/marcin/memory5/Memory5Application.java
@@ -0,0 +1,13 @@
+package gilko.marcin.memory5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Memory5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Memory5Application.class, args);
+	}
+
+}
---
+++ src/test/java/gilko/marcin/memory5/Memory5ApplicationTests.java
@@ -0,0 +1,13 @@
+package gilko.marcin.memory5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Memory5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
