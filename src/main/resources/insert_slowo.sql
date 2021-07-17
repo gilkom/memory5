813f6e3ee8647ac6fcb14b02f87af3f4ac486805
gko <gko@debian>
2021-07-17 11:58:45 +0200

    fix database insert

--- src/main/resources/insert_slowo.sql
+++ src/main/resources/insert_slowo.sql
@@ -10,168 +10,168 @@ INSERT INTO MEMO_SLOWO(slowo, tlumaczenie, rodzaj, dzwiek, obraz) VALUES(
 	'rower',
 	'rzeczownik',
 	'https://drive.google.com/file/d/1O-Zw740XMbYdym6ZHINgQOhU05CN4pW-/view?usp=sharing',
-	'https://drive.google.com/file/d/1oQXrmwc5ABKuJiSd-dOgkdAIzzL7T2zw/view?usp=sharing'
+	'https://drive.google.com/file/d/1Ve3hHgOrUc_qP-qGXYnWOHeYpttSdt9l/view?usp=sharing'
 );
 INSERT INTO MEMO_SLOWO(slowo, tlumaczenie, rodzaj, dzwiek, obraz) VALUES(
 	'bird',
 	'ptak',
 	'rzeczownik',
 	'https://drive.google.com/file/d/1O-Zw740XMbYdym6ZHINgQOhU05CN4pW-/view?usp=sharing',
-	'https://drive.google.com/file/d/1oQXrmwc5ABKuJiSd-dOgkdAIzzL7T2zw/view?usp=sharing'
+	'https://drive.google.com/file/d/1Z4WxySNrTOlTLzCWIVnzuEjBXcGEAqkF/view?usp=sharing'
 );
 INSERT INTO MEMO_SLOWO(slowo, tlumaczenie, rodzaj, dzwiek, obraz) VALUES(
 	'bug',
 	'robak',
 	'rzeczownik',
 	'https://drive.google.com/file/d/1O-Zw740XMbYdym6ZHINgQOhU05CN4pW-/view?usp=sharing',
-	'https://drive.google.com/file/d/1oQXrmwc5ABKuJiSd-dOgkdAIzzL7T2zw/view?usp=sharing'
+	'https://drive.google.com/file/d/1zMsh4OUuTsn5YHf9K3TSZG1jvh2sTOBQ/view?usp=sharing'
 );
 INSERT INTO MEMO_SLOWO(slowo, tlumaczenie, rodzaj, dzwiek, obraz) VALUES(
 	'car',
 	'samochód',
 	'rzeczownik',
 	'https://drive.google.com/file/d/1O-Zw740XMbYdym6ZHINgQOhU05CN4pW-/view?usp=sharing',
-	'https://drive.google.com/file/d/1oQXrmwc5ABKuJiSd-dOgkdAIzzL7T2zw/view?usp=sharing'
+	'https://drive.google.com/file/d/1qKJNKrEx02ISYVlFWVPainhbToH27fmh/view?usp=sharing'
 );
 INSERT INTO MEMO_SLOWO(slowo, tlumaczenie, rodzaj, dzwiek, obraz) VALUES(
 	'city',
 	'miasto',
 	'rzeczownik',
 	'https://drive.google.com/file/d/1O-Zw740XMbYdym6ZHINgQOhU05CN4pW-/view?usp=sharing',
-	'https://drive.google.com/file/d/1oQXrmwc5ABKuJiSd-dOgkdAIzzL7T2zw/view?usp=sharing'
+	'https://drive.google.com/file/d/1-r-4STJL5gyFVsZElFObH8w0hUF1WB1v/view?usp=sharing'
 );
 INSERT INTO MEMO_SLOWO(slowo, tlumaczenie, rodzaj, dzwiek, obraz) VALUES(
 	'cloud',
 	'chmura',
 	'rzeczownik',
 	'https://drive.google.com/file/d/1O-Zw740XMbYdym6ZHINgQOhU05CN4pW-/view?usp=sharing',
-	'https://drive.google.com/file/d/1oQXrmwc5ABKuJiSd-dOgkdAIzzL7T2zw/view?usp=sharing'
+	'https://drive.google.com/file/d/1hkk7XGYx_5cBRyOylp9S2xxuKRVinFd7/view?usp=sharing'
 );
 INSERT INTO MEMO_SLOWO(slowo, tlumaczenie, rodzaj, dzwiek, obraz) VALUES(
 	'contradiction',
 	'sprzeczność',
 	'rzeczownik',
 	'https://drive.google.com/file/d/1O-Zw740XMbYdym6ZHINgQOhU05CN4pW-/view?usp=sharing',
-	'https://drive.google.com/file/d/1oQXrmwc5ABKuJiSd-dOgkdAIzzL7T2zw/view?usp=sharing'
+	'https://drive.google.com/file/d/1BrbiP4PRItbHj7zWnxJp6FC1XIWky3Qd/view?usp=sharing'
 );
 INSERT INTO MEMO_SLOWO(slowo, tlumaczenie, rodzaj, dzwiek, obraz) VALUES(
 	'dog',
 	'pies',
 	'rzeczownik',
 	'https://drive.google.com/file/d/1O-Zw740XMbYdym6ZHINgQOhU05CN4pW-/view?usp=sharing',
-	'https://drive.google.com/file/d/1oQXrmwc5ABKuJiSd-dOgkdAIzzL7T2zw/view?usp=sharing'
+	'https://drive.google.com/file/d/18FkGv1kl6Cae25PFHKekoxRtcESqXSmQ/view?usp=sharing'
 );
 INSERT INTO MEMO_SLOWO(slowo, tlumaczenie, rodzaj, dzwiek, obraz) VALUES(
 	'doll',
 	'lalka',
 	'rzeczownik',
 	'https://drive.google.com/file/d/1O-Zw740XMbYdym6ZHINgQOhU05CN4pW-/view?usp=sharing',
-	'https://drive.google.com/file/d/1oQXrmwc5ABKuJiSd-dOgkdAIzzL7T2zw/view?usp=sharing'
+	'https://drive.google.com/file/d/1LqnRW1NEnV6vqaOXgTpqqKlJWnCuwyIz/view?usp=sharing'
 );
 INSERT INTO MEMO_SLOWO(slowo, tlumaczenie, rodzaj, dzwiek, obraz) VALUES(
 	'ear',
 	'ucho',
 	'rzeczownik',
 	'https://drive.google.com/file/d/1O-Zw740XMbYdym6ZHINgQOhU05CN4pW-/view?usp=sharing',
-	'https://drive.google.com/file/d/1oQXrmwc5ABKuJiSd-dOgkdAIzzL7T2zw/view?usp=sharing'
+	'https://drive.google.com/file/d/1VvdMPZhZ4cMbc6RPs9QRwhJD4d-0W_dd/view?usp=sharing'
 );
 INSERT INTO MEMO_SLOWO(slowo, tlumaczenie, rodzaj, dzwiek, obraz) VALUES(
 	'fast',
 	'szybko',
 	'rzeczownik',
 	'https://drive.google.com/file/d/1O-Zw740XMbYdym6ZHINgQOhU05CN4pW-/view?usp=sharing',
-	'https://drive.google.com/file/d/1oQXrmwc5ABKuJiSd-dOgkdAIzzL7T2zw/view?usp=sharing'
+	'https://drive.google.com/file/d/1Gr0l3VUfDS71uf09Xnlv6E2BOr0EFMxE/view?usp=sharing'
 );
 INSERT INTO MEMO_SLOWO(slowo, tlumaczenie, rodzaj, dzwiek, obraz) VALUES(
 	'good',
 	'dobry',
 	'rzeczownik',
 	'https://drive.google.com/file/d/1O-Zw740XMbYdym6ZHINgQOhU05CN4pW-/view?usp=sharing',
-	'https://drive.google.com/file/d/1oQXrmwc5ABKuJiSd-dOgkdAIzzL7T2zw/view?usp=sharing'
+	'https://drive.google.com/file/d/1YFYnfwqSCTTZW-baUTr5_-c_wkIQ6RTB/view?usp=sharing'
 );
 INSERT INTO MEMO_SLOWO(slowo, tlumaczenie, rodzaj, dzwiek, obraz) VALUES(
 	'house',
 	'dom',
 	'rzeczownik',
 	'https://drive.google.com/file/d/1O-Zw740XMbYdym6ZHINgQOhU05CN4pW-/view?usp=sharing',
-	'https://drive.google.com/file/d/1oQXrmwc5ABKuJiSd-dOgkdAIzzL7T2zw/view?usp=sharing'
+	'https://drive.google.com/file/d/18OWLMQCtNxlTqmXW9fKYxmymeEhvfr_b/view?usp=sharing'
 );
 INSERT INTO MEMO_SLOWO(slowo, tlumaczenie, rodzaj, dzwiek, obraz) VALUES(
 	'leg',
 	'noga',
 	'rzeczownik',
 	'https://drive.google.com/file/d/1O-Zw740XMbYdym6ZHINgQOhU05CN4pW-/view?usp=sharing',
-	'https://drive.google.com/file/d/1oQXrmwc5ABKuJiSd-dOgkdAIzzL7T2zw/view?usp=sharing'
+	'https://drive.google.com/file/d/1hcvOW5YthYPPDHJEV7nhuBnYXQ_RY5tT/view?usp=sharing'
 );
 INSERT INTO MEMO_SLOWO(slowo, tlumaczenie, rodzaj, dzwiek, obraz) VALUES(
 	'library',
 	'biblioteka',
 	'rzeczownik',
 	'https://drive.google.com/file/d/1O-Zw740XMbYdym6ZHINgQOhU05CN4pW-/view?usp=sharing',
-	'https://drive.google.com/file/d/1oQXrmwc5ABKuJiSd-dOgkdAIzzL7T2zw/view?usp=sharing'
+	'https://drive.google.com/file/d/1T7Kyyy1cc-uwWvY8IOC1IycbGpaCgswm/view?usp=sharing'
 );
 INSERT INTO MEMO_SLOWO(slowo, tlumaczenie, rodzaj, dzwiek, obraz) VALUES(
 	'lion',
 	'lew',
 	'rzeczownik',
 	'https://drive.google.com/file/d/1O-Zw740XMbYdym6ZHINgQOhU05CN4pW-/view?usp=sharing',
-	'https://drive.google.com/file/d/1oQXrmwc5ABKuJiSd-dOgkdAIzzL7T2zw/view?usp=sharing'
+	'https://drive.google.com/file/d/1nLlDjM9Uvy_YyINd3dAzp2XD9jHWeYvg/view?usp=sharing'
 );
 INSERT INTO MEMO_SLOWO(slowo, tlumaczenie, rodzaj, dzwiek, obraz) VALUES(
 	'pencil',
 	'ołówek',
 	'rzeczownik',
 	'https://drive.google.com/file/d/1O-Zw740XMbYdym6ZHINgQOhU05CN4pW-/view?usp=sharing',
-	'https://drive.google.com/file/d/1oQXrmwc5ABKuJiSd-dOgkdAIzzL7T2zw/view?usp=sharing'
+	'https://drive.google.com/file/d/1_SDcmOAVyi1puKqYeH2BgBb9bcQilcR3/view?usp=sharing'
 );
 INSERT INTO MEMO_SLOWO(slowo, tlumaczenie, rodzaj, dzwiek, obraz) VALUES(
 	'shop',
 	'sklep',
 	'rzeczownik',
 	'https://drive.google.com/file/d/1O-Zw740XMbYdym6ZHINgQOhU05CN4pW-/view?usp=sharing',
-	'https://drive.google.com/file/d/1oQXrmwc5ABKuJiSd-dOgkdAIzzL7T2zw/view?usp=sharing'
+	'https://drive.google.com/file/d/1-O2Qg6kg421Ewe3c4cUJVPmYgM691jlG/view?usp=sharing'
 );
 INSERT INTO MEMO_SLOWO(slowo, tlumaczenie, rodzaj, dzwiek, obraz) VALUES(
 	'street',
 	'ulica',
 	'rzeczownik',
 	'https://drive.google.com/file/d/1O-Zw740XMbYdym6ZHINgQOhU05CN4pW-/view?usp=sharing',
-	'https://drive.google.com/file/d/1oQXrmwc5ABKuJiSd-dOgkdAIzzL7T2zw/view?usp=sharing'
+	'https://drive.google.com/file/d/18puCxpVrssjMzGiI7QmlDH6iq4cX7Eik/view?usp=sharing'
 );
 INSERT INTO MEMO_SLOWO(slowo, tlumaczenie, rodzaj, dzwiek, obraz) VALUES(
 	'tiger',
 	'tygrys',
 	'rzeczownik',
 	'https://drive.google.com/file/d/1O-Zw740XMbYdym6ZHINgQOhU05CN4pW-/view?usp=sharing',
-	'https://drive.google.com/file/d/1oQXrmwc5ABKuJiSd-dOgkdAIzzL7T2zw/view?usp=sharing'
+	'https://drive.google.com/file/d/1yyLZTtD47ANaprKIUQvsJJfkBMSlCZ1I/view?usp=sharing'
 );
 INSERT INTO MEMO_SLOWO(slowo, tlumaczenie, rodzaj, dzwiek, obraz) VALUES(
 	'tree',
 	'drzewo',
 	'rzeczownik',
 	'https://drive.google.com/file/d/1O-Zw740XMbYdym6ZHINgQOhU05CN4pW-/view?usp=sharing',
-	'https://drive.google.com/file/d/1oQXrmwc5ABKuJiSd-dOgkdAIzzL7T2zw/view?usp=sharing'
+	'https://drive.google.com/file/d/1hy3nhRZi0e8y5iLMCaW-JV1T6rH6wSsz/view?usp=sharing'
 );
 INSERT INTO MEMO_SLOWO(slowo, tlumaczenie, rodzaj, dzwiek, obraz) VALUES(
 	'village',
 	'wieś',
 	'rzeczownik',
 	'https://drive.google.com/file/d/1O-Zw740XMbYdym6ZHINgQOhU05CN4pW-/view?usp=sharing',
-	'https://drive.google.com/file/d/1oQXrmwc5ABKuJiSd-dOgkdAIzzL7T2zw/view?usp=sharing'
+	'https://drive.google.com/file/d/18f4tpum3fHA3JjjCg20fPDHlybtWcNM2/view?usp=sharing'
 );
 INSERT INTO MEMO_SLOWO(slowo, tlumaczenie, rodzaj, dzwiek, obraz) VALUES(
 	'walking',
 	'chodzenie',
 	'rzeczownik',
 	'https://drive.google.com/file/d/1O-Zw740XMbYdym6ZHINgQOhU05CN4pW-/view?usp=sharing',
-	'https://drive.google.com/file/d/1oQXrmwc5ABKuJiSd-dOgkdAIzzL7T2zw/view?usp=sharing'
+	'https://drive.google.com/file/d/1rY7SgRFv-XVBGw_U2nMAlpKJ4k4Z5bOT/view?usp=sharing'
 );
 INSERT INTO MEMO_SLOWO(slowo, tlumaczenie, rodzaj, dzwiek, obraz) VALUES(
 	'woman',
 	'kobieta',
 	'rzeczownik',
 	'https://drive.google.com/file/d/1O-Zw740XMbYdym6ZHINgQOhU05CN4pW-/view?usp=sharing',
-	'https://drive.google.com/file/d/1oQXrmwc5ABKuJiSd-dOgkdAIzzL7T2zw/view?usp=sharing'
+	'https://drive.google.com/file/d/1aCoKlcrUjgLOu_ojNVEnjlNLzAypDND3/view?usp=sharing'
 );
 
 
